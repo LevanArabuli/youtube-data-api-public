6b14b0e7fa0dd75f81e8af72bc91415d0f3927c4
Levan Arabuli <arabuli91@gmail.com>
2018-03-08 20:38:30 +0400

    Add query for getting videos by channelId

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const {google} = require('googleapis');
 const key = require('./key.json');
 
+const youtube = google.youtube('v3');
+
 const jwtClient = new google.auth.JWT(
   key.client_email,
   null,
@@ -16,12 +18,46 @@ jwtClient.authorize(function(err, tokens) {
   }
 
   const videoId = 'Q4VGQPk2Dl8';
-
-  getVideoMetadata(jwtClient, videoId);
+  const channelId = 'UCBJycsmduvYEL83R_U4JriQ';
+  getVideosByChannel(jwtClient, channelId);
+  // getVideoMetadata(jwtClient, videoId);
 });
 
-function getVideoMetadata(auth, videoId) {
-  const youtube = google.youtube('v3');
+
+let videoIds = [];
+
+// Note that querying with channelId has limitation for 500 items
+// Find more on https://developers.google.com/youtube/v3/docs/search/list#channelId
+function getVideosByChannel(auth, channelId, pageToken){
+  youtube.search.list({
+    channelId: channelId,
+    auth: auth,
+    part: 'id',
+    type: 'video',
+    maxResults: 50,
+    pageToken: pageToken
+  }, function(err, response) {
+    if (err) {
+      console.log('The API returned an error: ' + err);
+      return;
+    }
+
+    const {data} = response;
+    const {nextPageToken} = data;
+    const {items} = data;
+
+    const ids = items.map(i => i.id.videoId);
+    videoIds = videoIds.concat(ids);
+
+    if(nextPageToken && nextPageToken.length > 0 && items.length > 0){
+      getVideosByChannel(auth, channelId, nextPageToken);
+    } else {
+      console.log('Done', videoIds.length)
+    }
+  });
+}
+
+function getVideoMetadata(auth) {
   youtube.videos.list(
     {
       id: videoId,
